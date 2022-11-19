2e3650d6c4fe585426bb52bc257017033ee7e83e
Kyle Van Gorkom <kyle.vangorkom@gmail.com>
2022-11-19 10:58:38 -0700

    add placeholder for AG UC8 MagAOX controller

---
+++ apps/aguc8Ctrl/aguc8Ctrl.cpp
@@ -0,0 +1,15 @@
+/** \file aguc8Ctrl.cpp
+  * \brief aguc8Ctrl source file, adapted from smc100ccCtrl.
+  * \author Kyle Van Gorkom
+  *
+  * \ingroup aguc8Ctrl_files
+  *
+  */
+#include "aguc8Ctrl.hpp"
+
+int main(int argc, char ** argv)
+{
+   MagAOX::app::saguc8Ctrl ctrl;
+
+   return ctrl.main(argc, argv);
+}
