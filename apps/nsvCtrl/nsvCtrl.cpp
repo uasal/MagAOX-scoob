04c13c7ecdeca14683cd7fdb505813bc0cbd53fd
Adam Schilperoort <adamschilperoort@arizona.edu>
2024-09-17 20:13:54 +0000

    initial stab at MagAOx camera app for NSV 571

---
+++ apps/nsvCtrl/nsvCtrl.cpp
@@ -0,0 +1,16 @@
+/** \file nsvCtrl.cpp
+  * \brief The MagAO-X xxxxx main program source file.
+  *
+  * \ingroup nsvCtrl_files
+  */
+
+#include "nsvCtrl.hpp"
+
+
+int main(int argc, char **argv)
+{
+   MagAOX::app::nsvCtrl xapp;
+
+   return xapp.main(argc, argv);
+
+}
