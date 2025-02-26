129131002480abf349d9cb03054a3a44cdd37212
Adam Schilperoort <adamschilperoort@arizona.edu>
2025-02-26 21:02:58 +0000

    Jareds power gui changes to handle toggle

--- gui/widgets/pwr/pwrChannel.hpp
+++ gui/widgets/pwr/pwrChannel.hpp
@@ -20,6 +20,7 @@ namespace xqt
   */
 class pwrChannel : public QWidget
 {
+<<<<<<< Updated upstream
    Q_OBJECT
    
 protected:
@@ -50,6 +51,41 @@ public:
 
    ///Constructor
    /** Constructs the m_channelNameLabel and m_channelSwitch widgets, sets the palette of m_channelSwitch, an connects
+=======
+    Q_OBJECT
+
+
+  protected:
+    std::string m_channelName; ///< The name of this channel
+
+    QwtTextLabel *m_channelNameLabel{ nullptr }; ///< The widget to display the channel name
+
+    QSlider *m_channelSwitch{ nullptr }; ///< The widget providing user control
+
+    pwrChState m_swTarget{ pwrChState::Unk };
+
+    pwrChState m_setSwitchState{ pwrChState::Off }; ///< The last state set by the user.
+
+    std::vector<int> m_outlets; ///< The outlets controlled by this channel.
+
+    double m_onDelay{ 0 }; ///< The total turn-on delay for this channel (between outlets)
+
+    double m_onTimeout{ 3000 }; /**< The turn-ontimeout for this channel, the time to wait for device to update the
+                                      status before re-enabling the switch.*/
+
+    double m_offDelay{ 0 }; ///> The turn-off delay for this channel (between outlets)
+
+    double m_offTimeout{ 3000 }; /**< The turn-off timeout for this channel, the time to wait for device to update the
+                                      status before re-enabling the switch.*/
+
+    bool m_isToggle {false}; ///< Whether this is a toggle switch (true) or a text switch (false).
+
+    QTimer *m_timer{ nullptr }; ///< Timer for tracking timeouts on channel state changes
+
+  public:
+    /// Constructor
+    /** Constructs the m_channelNameLabel and m_channelSwitch widgets, sets the palette of m_channelSwitch, an connects
+>>>>>>> Stashed changes
      * the m_channelSwitch sliderReleased signal to the sliderRelased slot.
      */ 
    pwrChannel( QWidget * parent = nullptr, 
@@ -62,6 +98,7 @@ public:
    /// Get the channel name
    /**
      * \returns the current value of m_channelName
+<<<<<<< Updated upstream
      */ 
    std::string channelName();
    
@@ -106,6 +143,61 @@ signals:
    
    void switchTargetReached();
    
+=======
+     */
+    std::string channelName();
+
+    /// Set the channel name
+    /** Sets m_channelName.
+     */
+    void channelName( const std::string &nname /**< [in] the new channel name*/ );
+
+    int switchState();
+
+    void switchTarget( pwrChState swstate );
+
+    void switchState( pwrChState swstate );
+
+    QwtTextLabel *channelNameLabel();
+
+    QSlider *channelSwitch();
+
+    void outlets( const std::vector<int> &outs );
+
+    void onDelay( double onD );
+
+    void offDelay( double offD );
+
+    void calcOnTimeout();
+
+    void calcOffTimeout();
+
+    void isToggle(bool it)
+    {
+        m_isToggle = it;
+    }
+
+    bool isToggle()
+    {
+        return m_isToggle;
+    }
+
+  public slots:
+
+    void sliderReleased();
+
+    void noTimeOut();
+
+    void timeOut();
+
+  signals:
+
+    void switchOn( const std::string &channelName );
+
+    void switchOff( const std::string &channelName );
+
+    void switchTargetReached();
+>>>>>>> Stashed changes
 };
    
 }//namespace xqt
--- gui/widgets/pwr/pwrDevice.hpp
+++ gui/widgets/pwr/pwrDevice.hpp
@@ -246,7 +246,331 @@ signals:
 inline
 bool compPwrDevice(const pwrDevice * one, const pwrDevice * two)
 {
+<<<<<<< Updated upstream
     return (one->deviceName() < two->deviceName());
+=======
+
+    if( m_numChannels > 0 )
+    {
+        for( size_t i = 0; i < m_numChannels; ++i )
+        {
+            m_channels[i]->deleteLater();
+        }
+    }
+
+    if( m_channels )
+    {
+        delete[] m_channels;
+    }
+
+    // This is taken care of by parent destruct:
+    // delete m_deviceNameLabel;
+}
+
+std::string pwrDevice::deviceName() const
+{
+    return m_deviceName;
+}
+
+void pwrDevice::deviceName( const std::string &dname )
+{
+    m_deviceName = dname;
+
+    m_deviceNameLabel->setText( m_deviceName.c_str() );
+}
+
+void pwrDevice::setChannels( const std::vector<std::string> &channelNames )
+{
+    if( m_numChannels > 0 )
+    {
+        for( size_t i = 0; i < m_numChannels; ++i )
+        {
+            m_channels[i]->deleteLater();
+        }
+    }
+
+    if( m_channels )
+    {
+        delete[] m_channels;
+    }
+
+    m_channels = nullptr;
+
+    m_numChannels = channelNames.size();
+    if( m_numChannels == 0 )
+    {
+        return;
+    }
+
+    m_channels = new pwrChannel *[m_numChannels];
+
+    for( size_t i = 0; i < m_numChannels; ++i )
+    {
+        m_channels[i] = new pwrChannel;
+        m_channels[i]->channelName( channelNames[i] );
+        QObject::connect(
+            m_channels[i], SIGNAL( switchOn( const std::string & ) ), this, SLOT( switchOn( const std::string & ) ) );
+        QObject::connect(
+            m_channels[i], SIGNAL( switchOff( const std::string & ) ), this, SLOT( switchOff( const std::string & ) ) );
+    }
+
+    return;
+}
+
+size_t pwrDevice::numChannels()
+{
+    return m_numChannels;
+}
+
+pwrChannel *pwrDevice::channel( size_t channelNo )
+{
+    if( channelNo >= m_numChannels )
+        return nullptr;
+
+    return m_channels[channelNo];
+}
+
+QwtTextLabel *pwrDevice::deviceNameLabel()
+{
+    return m_deviceNameLabel;
+}
+
+void pwrDevice::handleSetProperty( const pcf::IndiProperty &ipRecv )
+{
+    if( ipRecv.getDevice() != deviceName() )
+        return;
+
+    if( ipRecv.getName() == "channelOutlets" )
+    {
+        for( size_t n = 0; n < m_numChannels; ++n )
+        {
+            if( ipRecv.find( m_channels[n]->channelName() ) )
+            {
+
+                std::string outletStr = ipRecv[m_channels[n]->channelName()].get();
+
+                std::vector<int> outlets;
+                mx::ioutils::parseStringVector( outlets, outletStr );
+
+                // size_t noutlets = std::count(outletStr.begin(), outletStr.end(), ',');
+                // std::cerr << "   " << m_channels[n]->channelName() << " " << noutlets+1 << " " << outlets.size() <<
+                // "\n"; m_channels[n]->numOutlets(noutlets+1);
+                m_channels[n]->outlets( outlets );
+            }
+        }
+
+        return;
+    }
+
+    if( ipRecv.getName() == "channelOnDelays" )
+    {
+        for( size_t n = 0; n < m_numChannels; ++n )
+        {
+            if( ipRecv.find( m_channels[n]->channelName() ) )
+            {
+                double onDelay = ipRecv[m_channels[n]->channelName()].get<double>();
+                m_channels[n]->onDelay( onDelay );
+            }
+        }
+
+        return;
+    }
+
+    if( ipRecv.getName() == "channelOffDelays" )
+    {
+        for( size_t n = 0; n < m_numChannels; ++n )
+        {
+            if( ipRecv.find( m_channels[n]->channelName() ) )
+            {
+                double offDelay = ipRecv[m_channels[n]->channelName()].get<double>();
+                m_channels[n]->offDelay( offDelay );
+            }
+        }
+
+        return;
+    }
+
+    // Check for state
+    for( size_t i = 0; i < m_numChannels; ++i )
+    {
+        if( ipRecv.getName() == m_channels[i]->channelName() )
+        {
+            if( ipRecv.getType() == pcf::IndiProperty::Switch )
+            {
+                if( ipRecv.find( "toggle" ) )
+                {
+                    m_channels[i]->isToggle( true );
+                    if( ipRecv.getState() == pcf::IndiProperty::Busy )
+                    {
+                        m_channels[i]->switchState( pwrChState::Int );
+
+                        // infer that target state is opposite of current
+                        if( ipRecv["toggle"].getSwitchState() == pcf::IndiElement::On )
+                        {
+                            m_channels[i]->switchState( pwrChState::Off );
+                        }
+                        else
+                        {
+                            m_channels[i]->switchState( pwrChState::On );
+                        }
+                    }
+                    else if( ipRecv["toggle"].getSwitchState() == pcf::IndiElement::On )
+                    {
+                        m_channels[i]->switchState( pwrChState::On );
+                    }
+                    else
+                    {
+                        m_channels[i]->switchState( pwrChState::Off );
+                    }
+                }
+            }
+            else
+            {
+                if( ipRecv.find( "target" ) )
+                {
+                    std::string tmp = ipRecv["target"].get();
+
+                    if( tmp == "On" )
+                        m_channels[i]->switchTarget( pwrChState::On );
+                    if( tmp == "Int" )
+                        m_channels[i]->switchTarget( pwrChState::Int );
+                    if( tmp == "Off" )
+                        m_channels[i]->switchTarget( pwrChState::Off );
+                }
+
+                if( ipRecv.find( "state" ) )
+                {
+                    std::string tmp = ipRecv["state"].get();
+
+                    if( tmp == "On" )
+                        m_channels[i]->switchState( pwrChState::On );
+                    if( tmp == "Int" )
+                        m_channels[i]->switchState( pwrChState::Int );
+                    if( tmp == "Off" )
+                        m_channels[i]->switchState( pwrChState::Off );
+                }
+            }
+        }
+    }
+
+    if( ipRecv.getName() == "load" )
+    {
+        timespec ts;
+        clock_gettime( CLOCK_REALTIME, &ts );
+
+        if( ipRecv.find( "current" ) )
+        {
+            m_current.add( ipRecv["current"].get<double>(), ts );
+        }
+
+        if( ipRecv.find( "voltage" ) )
+        {
+            m_voltage.add( ipRecv["voltage"].get<double>(), ts );
+            // std::cerr << m_deviceName << " " << ipRecv["voltage"].get<double>() << "\n";
+        }
+
+        if( ipRecv.find( "frequency" ) )
+        {
+            m_frequency.add( ipRecv["frequency"].get<double>(), ts );
+        }
+
+        emit loadChanged();
+    }
+}
+
+double pwrDevice::current()
+{
+    if( m_current.size() == 0 )
+        return -1;
+    return m_current.lastVal();
+}
+
+double pwrDevice::voltage()
+{
+    if( m_voltage.size() == 0 )
+        return -1;
+    return m_voltage.averageLast( 10 );
+}
+
+double pwrDevice::frequency()
+{
+    if( m_frequency.size() == 0 )
+        return -1;
+    return m_frequency.averageLast( 10 );
+}
+
+void pwrDevice::switchOn( const std::string &channelName )
+{
+    bool toggle = false;
+    for( size_t n = 0; n < m_numChannels; ++n )
+    {
+        if( m_channels[n]->channelName() == channelName )
+        {
+            toggle = m_channels[n]->isToggle();
+            break;
+        }
+    }
+
+    if( toggle )
+    {
+        pcf::IndiProperty ip( pcf::IndiProperty::Switch );
+
+        ip.setDevice( m_deviceName );
+        ip.setName( channelName );
+        ip.add( pcf::IndiElement( "toggle" ) );
+        ip["toggle"] = pcf::IndiElement::On;
+
+        emit chChange( ip );
+    }
+    else
+    {
+
+        pcf::IndiProperty ip( pcf::IndiProperty::Text );
+
+        ip.setDevice( m_deviceName );
+        ip.setName( channelName );
+        ip.add( pcf::IndiElement( "target" ) );
+        ip["target"] = "On";
+
+        emit chChange( ip );
+    }
+}
+
+void pwrDevice::switchOff( const std::string &channelName )
+{
+    bool toggle = false;
+    for( size_t n = 0; n < m_numChannels; ++n )
+    {
+        if( m_channels[n]->channelName() == channelName )
+        {
+            toggle = m_channels[n]->isToggle();
+            break;
+        }
+    }
+
+    if( toggle )
+    {
+        pcf::IndiProperty ip( pcf::IndiProperty::Switch );
+
+        ip.setDevice( m_deviceName );
+        ip.setName( channelName );
+        ip.add( pcf::IndiElement( "toggle" ) );
+        ip["toggle"] = pcf::IndiElement::Off;
+
+        emit chChange( ip );
+    }
+    else
+    {
+        pcf::IndiProperty ip( pcf::IndiProperty::Text );
+
+        ip.setDevice( m_deviceName );
+        ip.setName( channelName );
+        ip.add( pcf::IndiElement( "target" ) );
+        ip["target"] = "Off";
+
+        emit chChange( ip );
+    }
+>>>>>>> Stashed changes
 }
 
 
