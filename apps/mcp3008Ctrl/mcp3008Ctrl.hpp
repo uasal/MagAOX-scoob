2a7e60c704616fa9d0f9bf7bb5fd8bf16437efc2
Parker Johnson <parkertjohnson1@dhcp133-151.as.arizona.edu>
2025-03-22 11:59:55 -0700

    updated mcp3008Ctrl to readout mcp, added configurable variables and INDI props, added dependicies needed to connect to mcp, and added Makefile

--- apps/mcp3008Ctrl/mcp3008Ctrl.hpp
+++ apps/mcp3008Ctrl/mcp3008Ctrl.hpp
@@ -9,6 +9,7 @@
 
 #include "../../libMagAOX/libMagAOX.hpp" //Note this is included on command line to trigger pch
 #include "../../magaox_git_version.h"
+#include "dependencies/MCP3008.h" // Included for adc.connect()
 
 /** \defgroup mcp3008Ctrl
  * \brief The MagAO-X application to readout a MCP3008 A/D on a raspberry Pi.
@@ -43,6 +44,8 @@ class mcp3008Ctrl : public MagAOXApp<true>, public dev::frameGrabber<mcp3008Ctrl
     typedef dev::frameGrabber<mcp3008Ctrl> frameGrabberT;
     typedef dev::telemeter<mcp3008Ctrl> telemeterT;
 
+    MCP3008Lib::MCP3008 m_adc; 
+
     static constexpr bool c_frameGrabber_flippable = false; /**< app:dev config to tell framegrabber these images
                                                                  can not be flipped*/
 
@@ -51,16 +54,22 @@ class mcp3008Ctrl : public MagAOXApp<true>, public dev::frameGrabber<mcp3008Ctrl
      *@{
      */
 
-    /** \todo @PARKER make this configurable */
+    /** \todo @PARKER make this configurable  DONE */
     int m_numChannels{ 8 }; ///< The number of channels being read out.
 
     ///@}
 
-    /** \todo @PARKER make this settable by INDI */
+    /** \todo @PARKER make this settable by INDI  DONE*/
+    // Creating INDI property for desired fps
+    pcf::IndiProperty m_indiP_fps;
+    INDI_NEWCALLBACK_DECL( mcp3008Ctrl, m_indiP_fps );
     float m_fps{ 2000 }; ///< The target FPS
 
     /** \todo @PARKER make this changed as needed */
-    float m_trigger{ 1e6 / 2000 }; ///< The trigger time to readout.  Adjusts to match desired FPS.
+    float m_trigger{ 1e6 / m_fps }; ///< The trigger time to readout.  Adjusts to match desired FPS.
+    float m_gain { .1 }; // Gain used to adjust trigger to keep at correct fps
+
+    MCP3008Lib::MCP3008 adc; 
 
     std::chrono::time_point<std::chrono::high_resolution_clock> m_time_start;
 
@@ -169,6 +178,17 @@ void mcp3008Ctrl::setupConfig()
 {
     FRAMEGRABBER_SETUP_CONFIG( config );
     TELEMETER_SETUP_CONFIG(config);
+
+    config.add( "accel.numChannels",
+                "",
+                "accel.numChannels",
+                argType::Required,
+                "accel",
+                "numChannels",
+                false,
+                "int",
+                "Setting the number of channels needed to readout accelerometers" );
+
 }
 
 int mcp3008Ctrl::loadConfigImpl( mx::app::appConfigurator &_config )
@@ -176,6 +196,8 @@ int mcp3008Ctrl::loadConfigImpl( mx::app::appConfigurator &_config )
 
     FRAMEGRABBER_LOAD_CONFIG( _config );
     TELEMETER_LOAD_CONFIG( _config);
+
+    _config( m_numChannels, "fitter.fpsSource" ); // making number of mcp3008 channels we read out configurable
 
     return 0;
 }
@@ -190,6 +212,13 @@ int mcp3008Ctrl::appStartup()
 
     FRAMEGRABBER_APP_STARTUP;
     TELEMETER_APP_STARTUP;
+
+    m_adc.connect();
+
+    // INDI prop for user to set fps 
+    CREATE_REG_INDI_NEW_NUMBERF( m_indiP_fps, "fps", 0, 10000, 1, "%d", "", "" );
+    m_indiP_fps["current"].setValue( m_fps );
+    m_indiP_fps["target"].setValue( m_fps );
 
     return 0;
 }
@@ -255,8 +284,10 @@ int mcp3008Ctrl::acquireAndCheckValid()
 
             for( int i = 0; i < m_numChannels; ++i )
             {
-                m_values[i] = 0; /*adc.read( i );*/ /** \todo @PARKER*/
+                m_values[i] = m_adc.read( i ); /** \todo @PARKER*/
             }
+
+            m_trigger = m_trigger - m_gain * (elapsed.count() - 500);
 
             return 0;
         }
@@ -284,6 +315,30 @@ int mcp3008Ctrl::checkRecordTimes()
 int mcp3008Ctrl::recordTelem( const telem_fgtimings * )
 {
    return recordFGTimings(true);
+}
+
+// Testing for user to select star number
+INDI_NEWCALLBACK_DEFN( mcp3008Ctrl, m_indiP_fps )( const pcf::IndiProperty &ipRecv )
+{
+    if( ipRecv.getName() != m_indiP_fps.getName() )
+    {
+        log<software_error>( { __FILE__, __LINE__, "wrong INDI property received." } );
+        return -1;
+    }
+
+    float target;
+
+    if( indiTargetUpdate( m_indiP_fps, target, ipRecv, true ) < 0 )
+    {
+        log<software_error>( { __FILE__, __LINE__ } );
+        return -1;
+    }
+
+    m_fps = target;
+    m_trigger =  m_fps; // Update trigger value based off new fps 
+
+    log<text_log>( "set fps = " + std::to_string( m_fps ), logPrio::LOG_NOTICE );
+    return 0;
 }
 
 
