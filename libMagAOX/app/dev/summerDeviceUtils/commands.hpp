8fe1de49c1dced65848eed1b33d9b6417629886c
I Stefan <istefan@arizona.edu>
2025-05-08 18:45:32 +0100

    Added ShortPixelsQuery dm command.
    Also added VersionQuery to summerDevice template

--- libMagAOX/app/dev/summerDeviceUtils/commands.hpp
+++ libMagAOX/app/dev/summerDeviceUtils/commands.hpp
@@ -18,7 +18,23 @@ namespace MagAOX
         namespace dev
         {
             /**
-             * @brief Base class for all the fsm queries
+             * @brief Payload types common to all devices
+             *
+             * The payload type is sent with the command packet and
+             * tells the device what command is being sent.
+             */
+            static const uint16_t CGraphPayloadTypeVersion = 0x1001U;
+
+            struct CGraphVersionPayload
+            {
+                uint32_t SerialNum;
+                uint32_t ProcessorFirmwareBuildNum;
+                uint32_t FPGAFirmwareBuildNum;
+            };
+
+
+            /**
+             * @brief Base class for all the summerDevice queries
              *
              * sdevQuery is the class from which all the query classes inherit.
              * It ensures that the all implement a minimal interfaces that includes
@@ -63,6 +79,55 @@ namespace MagAOX
                 size_t DefaultPayloadLen = 0;
                 void *PayloadData = DefaultPayloadData;
                 size_t PayloadLen = DefaultPayloadLen;
+            };
+
+
+            // Derived classes
+            /**
+             * @brief Child query class that handles sending a version query to the device
+             */
+            class VersionQuery : public dev::sdevQuery
+            {
+            public:
+                VersionQuery()
+                {
+                    PayloadType = CGraphPayloadTypeVersion;
+                    startLog = "PZTVersion: Querying version.";
+                    endLog = "PZTVersion: Finished querying version.";
+                }
+
+                const CGraphVersionPayload *ParamsPtr = nullptr;
+                CGraphVersionPayload Version;
+
+                void processReply(char const *Params, const size_t ParamsLen) override
+                {
+                    if ((NULL != Params) && (ParamsLen >= (sizeof(CGraphVersionPayload))) )
+                    {
+                        ParamsPtr = reinterpret_cast<const CGraphVersionPayload *>(Params);
+                        Version = *ParamsPtr;
+                    }
+                    else
+                    {
+                        errorLogString(ParamsLen);
+                    }
+                }
+
+                void errorLogString(const size_t ParamsLen) override
+                {
+                    std::ostringstream oss;
+                    oss << "PZTVersion: Short packet: " << ParamsLen << " (expected " << sizeof(CGraphVersionPayload) << " bytes): ";
+                    MagAOX::app::MagAOXApp<true>::log<software_error>({__FILE__, __LINE__, oss.str()});
+                }
+
+                void logReply() override
+                {
+                    std::ostringstream oss;
+                    oss << "PZTVersion Command: Version received\n";
+                    // debug
+                    oss << "PZTVersion Command: \n";
+                    oss << "CGraphVersionPayload: SerialNum: 0x " << Version.SerialNum << ", ProcessorFirmwareBuildNum: " << Version.ProcessorFirmwareBuildNum << ", FPGAFirmwareBuildNum: " << Version.FPGAFirmwareBuildNum  << "\n";
+                    MagAOX::app::MagAOXApp<true>::log<text_log>(oss.str());
+                }
             };
         } // namespace dev
     } // namespace app
