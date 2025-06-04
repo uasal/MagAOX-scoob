70d15924c1a74b88f3be1ca319a549b754880be3
Adam Schilperoort <adamschilperoort@gmail.com>
2025-06-04 14:48:02 -0700

    update asiCtrl camera to use serial number

--- apps/asiCtrl/cleanASI.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-###########################################################
-#
-# Clean up various Picam and pleora lock and state
-# files which can prevent startup of a new process
-#
-###########################################################
