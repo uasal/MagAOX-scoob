561d66c6eb6560e979a3cd79a0b5dc5d38802ea9
Joseph Long <github@joseph-long.com>
2025-11-26 11:07:08 -0300

    Untangle the Makefile a bit, split pythonapps_install off

--- Make/pythonApp.mk
+++ Make/pythonApp.mk
@@ -7,8 +7,8 @@ all:
 	@echo "*** $(TARGET) is a Python app, skipping 'build' step. Use 'make install' to install. ***"
 
 install:
-	sudo -H $(PYTHON) -c 'import purepyindi2' || (echo "Need purepyindi2 installed to $(shell which python)" && exit 1)
-	sudo -H $(PYTHON) -c 'import xconf' || (echo "Need xconf installed to $(shell which python)" && exit 1)
+	$(PYTHON) -c 'import purepyindi2' || (echo "Need purepyindi2 installed to $(PYTHON)" && exit 1)
+	$(PYTHON) -c 'import xconf' || (echo "Need xconf installed to $(PYTHON)" && exit 1)
 	sudo -H $(PYTHON) -m pip install .
 	sudo -H ln -sfv $(PYTHON_SCRIPTS_PREFIX)/$(TARGET) /opt/MagAOX/bin/$(TARGET)
 	@echo "*** Install succeeded, app located in /opt/MagAOX/bin/$(TARGET) ***"
