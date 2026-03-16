4a00cfffab9bc4e5683b74049920187cb06a0b2e
I Stefan <istefan@arizona.edu>
2026-03-16 22:43:00 +0000

    Added getAccessor to fsm and fsm telem headers

--- libMagAOX/logger/types/telem_dm.hpp
+++ libMagAOX/logger/types/telem_dm.hpp
@@ -106,6 +106,97 @@ struct telem_dm : public flatbuffer_log
 
    }
 
+    static double P1V2( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P1V2();
+    }
+
+    static double P2V2( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P2V2();
+    }
+
+    static double P28V( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P28V();
+    }
+
+    static double P2V5( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P2V5();
+    }
+
+    static double P6V( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P6V();
+    }
+
+    static double P5V( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P5V();
+    }
+
+    static double P3V3D( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P3V3D();
+    }
+
+    static double P4V3( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P4V3();
+    }
+
+    static double P2I2( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P2I2();
+    }
+
+    static double P4I3( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P4I3();
+    }
+
+    static double P6I( void * msgBuffer )
+    {
+      auto fbs = GetTelem_dm_fb(msgBuffer);
+      return fbs->P6I();
+    }
+
+    /// Get the logMetaDetail for a member by name
+    /**
+      * \returns the logMetaDetail filled in with the appropriate details
+      * \returns an empty logMetaDetail if member not recognized
+      */
+    static logMetaDetail getAccessor( const std::string & member /**< [in] the name of the member */ )
+    {
+      if(member == "P1V2") return logMetaDetail({"P1V2", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P1V2), false});
+      else if(member == "P2V2") return logMetaDetail({"P2V2", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P2V2), false});
+      else if(member == "P28V") return logMetaDetail({"P28V", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P28V), false});
+      else if(member == "P2V5") return logMetaDetail({"P2V5", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P2V5), false});
+      else if(member == "P6V") return logMetaDetail({"P6V", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P6V), false});
+      else if(member == "P5V") return logMetaDetail({"P5V", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P5V), false});
+      else if(member == "P3V3D") return logMetaDetail({"P3V3D", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P3V3D), false});
+      else if(member == "P4V3") return logMetaDetail({"P4V3", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P4V3), false});
+      else if(member == "P2I2") return logMetaDetail({"P2I2", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P2I2), false});
+      else if(member == "P4I3") return logMetaDetail({"P4I3", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P4I3), false});
+      else if(member == "P6I") return logMetaDetail({"P6I", "", logMeta::valTypes::Double, logMeta::metaTypes::Continuous, reinterpret_cast<void*>(&P6I), false});
+      else
+      {
+        std::cerr << "No member " << member << " in telem_dm\n";
+        return logMetaDetail();
+      }
+    }
+
 }; //telem_dm
 
 
