d6d550f2e59efe479f5d9c94fc20532629b9ac91
Jared Males <jaredmales@gmail.com>
2026-03-22 22:25:55 -0300

    cleaned up vestigial indi properties in w2tcsOffloader

--- apps/w2tcsOffloader/w2tcsOffloader.hpp
+++ apps/w2tcsOffloader/w2tcsOffloader.hpp
@@ -62,8 +62,7 @@ protected:
    std::string m_wMaskPath;
    std::vector<std::string> m_elNames;
    std::vector<realT> m_zCoeffs;
-   float m_gain {0.1};
-   int m_nModes {2};
+   unsigned m_nModes {5};
    float m_norm {1.0};
 
    ///@}
@@ -120,16 +119,11 @@ public:
 
 protected:
 
-   pcf::IndiProperty m_indiP_gain;
    pcf::IndiProperty m_indiP_nModes;
    pcf::IndiProperty m_indiP_zCoeffs;
 
-   pcf::IndiProperty m_indiP_zero;
 
-   INDI_NEWCALLBACK_DECL(w2tcsOffloader, m_indiP_gain);
-   INDI_NEWCALLBACK_DECL(w2tcsOffloader, m_indiP_nModes);
-   INDI_NEWCALLBACK_DECL(w2tcsOffloader, m_indiP_zero);
-   INDI_NEWCALLBACK_DECL(w2tcsOffloader, m_indiP_zCoeffs);
+
 };
 
 inline
@@ -145,7 +139,6 @@ void w2tcsOffloader::setupConfig()
 
    config.add("offload.wZModesPath", "", "offload.wZModesPath", argType::Required, "offload", "wZModesPath", false, "string", "The path to the woofer Zernike modes.");
    config.add("offload.wMaskPath", "", "offload.wMaskPath", argType::Required, "offload", "wMaskPath", false, "string", "Path to the woofer Zernike mode mask.");
-   config.add("offload.gain", "", "offload.gain", argType::Required, "offload", "gain", false, "float", "The starting offload gain.  Default is 0.1.");
    config.add("offload.nModes", "", "offload.nModes", argType::Required, "offload", "nModes", false, "int", "Number of modes to offload to the TCS.");
 }
 
@@ -157,7 +150,6 @@ int w2tcsOffloader::loadConfigImpl( mx::app::appConfigurator & _config )
 
    _config(m_wZModesPath, "offload.wZModesPath");
    _config(m_wMaskPath, "offload.wMaskPath");
-   _config(m_gain, "offload.gain");
    _config(m_nModes, "offload.nModes");
 
    return 0;
@@ -189,27 +181,13 @@ int w2tcsOffloader::appStartup(){
 
    m_norm = m_wMask.sum();
 
-   createStandardIndiNumber<unsigned>( m_indiP_gain, "gain", 0, 1, 0, "%0.2f");
-   m_indiP_gain["current"] = m_gain;
-   m_indiP_gain["target"] = m_gain;
-
-   if( registerIndiPropertyNew( m_indiP_gain, INDI_NEWCALLBACK(m_indiP_gain)) < 0)
-   {
-      log<software_error>({__FILE__,__LINE__});
-      return -1;
-   }
-
-   createStandardIndiNumber<unsigned>( m_indiP_nModes, "nModes", 1, std::numeric_limits<unsigned>::max(), 1, "%u");
+   createROIndiNumber( m_indiP_nModes, "nModes", "number of modes calculated");
+   indi::addNumberElement<unsigned>( m_indiP_nModes, "current", 0, m_zCoeffs.size(), 1, "%d");
    m_indiP_nModes["current"] = m_nModes;
 
-   if( registerIndiPropertyNew( m_indiP_nModes, INDI_NEWCALLBACK(m_indiP_nModes)) < 0)
-   {
-      log<software_error>({__FILE__,__LINE__});
-      return -1;
-   }
+   registerIndiPropertyReadOnly(m_indiP_nModes);
 
-   REG_INDI_NEWPROP(m_indiP_zCoeffs, "zCoeffs", pcf::IndiProperty::Number);
-
+   registerIndiPropertyReadOnly(m_indiP_zCoeffs);
 
    m_elNames.resize(m_zCoeffs.size());
    for(size_t n=0; n < m_zCoeffs.size(); ++n)
@@ -224,14 +202,6 @@ int w2tcsOffloader::appStartup(){
    if(shmimMonitorT::appStartup() < 0)
    {
       return log<software_error,-1>({__FILE__, __LINE__});
-   }
-
-
-   createStandardIndiRequestSw( m_indiP_zero, "zero", "zero loop");
-   if( registerIndiPropertyNew( m_indiP_zero, INDI_NEWCALLBACK(m_indiP_zero)) < 0)
-   {
-      log<software_error>({__FILE__,__LINE__});
-      return -1;
    }
 
    state(stateCodes::OPERATING);
@@ -303,7 +273,7 @@ int w2tcsOffloader::processImage( void * curr_src,
     {
        float coeff;
        coeff = ( Eigen::Map<mx::improc::eigenImage<realT>>((float *)curr_src, shmimMonitorT::m_width, shmimMonitorT::m_height) * m_wZModes.image(i) * m_wMask).sum() / m_norm;
-       m_indiP_zCoeffs[m_elNames[i]] = m_gain * coeff;
+       m_indiP_zCoeffs[m_elNames[i]] = coeff;
     }
     else
     {
@@ -322,84 +292,6 @@ int w2tcsOffloader::processImage( void * curr_src,
    return 0;
 }
 
-// update this: mode coefficients (maybe they shouldn't be settable. How to handle?)
-
-INDI_NEWCALLBACK_DEFN(w2tcsOffloader, m_indiP_gain)(const pcf::IndiProperty &ipRecv)
-{
-    INDI_VALIDATE_CALLBACK_PROPS(m_indiP_gain, ipRecv);
-
-    float target;
-
-    if( indiTargetUpdate( m_indiP_gain, target, ipRecv, true) < 0)
-    {
-        log<software_error>({__FILE__,__LINE__});
-        return -1;
-    }
-
-    m_gain = target;
-
-    updateIfChanged(m_indiP_gain, "current", m_gain);
-    updateIfChanged(m_indiP_gain, "target", m_gain);
-
-    log<text_log>("set gain to " + std::to_string(m_gain), logPrio::LOG_NOTICE);
-
-    return 0;
-}
-
-INDI_NEWCALLBACK_DEFN(w2tcsOffloader, m_indiP_nModes)(const pcf::IndiProperty &ipRecv)
-{
-    INDI_VALIDATE_CALLBACK_PROPS(m_indiP_nModes, ipRecv);
-
-    unsigned target;
-
-    if( indiTargetUpdate( m_indiP_nModes, target, ipRecv, true) < 0)
-    {
-        log<software_error>({__FILE__,__LINE__});
-        return -1;
-    }
-
-    m_nModes = target;
-
-    updateIfChanged(m_indiP_nModes, "current", m_nModes);
-    updateIfChanged(m_indiP_nModes, "target", m_nModes);
-
-    log<text_log>("set nModes to " + std::to_string(m_nModes), logPrio::LOG_NOTICE);
-
-    return 0;
-}
-
-INDI_NEWCALLBACK_DEFN(w2tcsOffloader, m_indiP_zCoeffs)(const pcf::IndiProperty &ipRecv)
-{
-    INDI_VALIDATE_CALLBACK_PROPS(m_indiP_zCoeffs, ipRecv);
-
-
-    for(size_t n=0; n < m_zCoeffs.size(); ++n)
-    {
-        if(ipRecv.find(m_elNames[n]))
-        {
-            realT zcoeff = ipRecv[m_elNames[n]].get<realT>();
-            m_zCoeffs[n] = zcoeff;
-        }
-    }
-    return 0;
-
-
-   return log<software_error,-1>({__FILE__,__LINE__, "invalid indi property name"});
-}
-
-INDI_NEWCALLBACK_DEFN(w2tcsOffloader, m_indiP_zero)(const pcf::IndiProperty &ipRecv)
-{
-    INDI_VALIDATE_CALLBACK_PROPS(m_indiP_zero, ipRecv);
-
-    float target;
-
-    if( ipRecv["toggle"].getSwitchState() == pcf::IndiElement::On)
-    {
-        m_woofer.setZero();
-        log<text_log>("set zero", logPrio::LOG_NOTICE);
-    }
-    return 0;
-}
 
 } //namespace app
 } //namespace MagAOX
