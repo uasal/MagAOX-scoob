e5b9a45e16921d903a71469f6772eedc52d01d5d
Adam Schilperoort <adamschilperoort@arizona.edu>
2026-04-09 17:25:54 -0700

    further edits to kim101

--- apps/kim101Ctrl/kim101Ctrl.cpp
+++ apps/kim101Ctrl/kim101Ctrl.cpp
@@ -4,11 +4,18 @@
   * \ingroup kim101Ctrl_files
   */
 
+#include <csignal>
+
 #include "kim101Ctrl.hpp"
 
 
 int main(int argc, char **argv)
 {
+   // mxlib MagAOXApp uses resurrectee, which writes to a pipe for the parent
+   // resurrector. When the binary is run alone (e.g. gdb), that write raises
+   // SIGPIPE; ignore it so local debugging works (write fails with EPIPE instead).
+   std::signal(SIGPIPE, SIG_IGN);
+
    MagAOX::app::kim101Ctrl xapp;
 
    return xapp.main(argc, argv);
